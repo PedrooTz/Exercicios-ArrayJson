b11ff9722c6358a69c0e6354037ac833f19e2b4d
Pedro Henrique <pedrohenrique1603@gmail.com>
2023-10-20 13:43:25 -0300

    Em andamento

---
+++ main.js
@@ -0,0 +1,45 @@
+const estadosCidades =require('./modulo/estados_cidades')
+
+// const getListaDeEstados = function(){
+
+//     let ArraySigla = []
+//     let JsonSigla = {}
+//     let cont = 0;
+//     while  (cont < 27 ){
+
+//     ArraySigla.push(estadosCidades.estadosCidades.estados[cont].sigla)
+//     cont++
+//     }
+//     JsonSigla.quantidade = ArraySigla.length
+//     JsonSigla.uf = ArraySigla
+//     return JsonSigla
+
+
+// }
+
+    const getDadosEstado = function(){
+        
+        let DadosJson = {}
+        let FiltrarEstados = 'SP'
+        let cont = 0;
+        while  (cont < true ){
+
+            if(FiltrarEstados)
+
+        ArraySigla.push(estadosCidades.estadosCidades.sigla, nome, capital, regiao[cont])
+        cont++
+    
+        }
+
+        JsonSigla.uf = ArraySigla
+        
+        return JsonSigla
+
+    }
+
+    console.log(getDadosEstado())
+
+
+// console.log(getListaDeEstados())
+
+
